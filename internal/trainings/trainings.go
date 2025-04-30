264ceed3d0d1e0148bc8e43d4df78f234311414c
Eliz Kotova <elizmermaid@gmail.com>
2025-05-01 01:27:31 +0300

    add err messages and fix some code

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -27,6 +27,9 @@ func (t *Training) Parse(datastring string) (err error) {
 	if err != nil {                      //Обработать возможные ошибки.
 		return fmt.Errorf("error parsing number of steps: %v", err) //При возникновении ошибки вернуть её из метода.
 	}
+	if steps <= 0 {
+		return fmt.Errorf("number of steps must be positive")
+	}
 	t.Steps = steps //Сохранить полученное значение в соответствующем поле структуры Training.
 
 	t.TrainingType = parts[1] //Сохранить значение типа тренировки в соответствующем поле структуры Training.
@@ -34,6 +37,9 @@ func (t *Training) Parse(datastring string) (err error) {
 	duration, err := time.ParseDuration(parts[2]) //Преобразовать третий  элемент слайса в time.Duration.
 	if err != nil {                               //Обработать возможные ошибки.
 		return fmt.Errorf("error parsing duration: %v", err) //При их возникновении вернуть ошибку.
+	}
+	if duration <= 0 {
+		return fmt.Errorf("duration must be positive")
 	}
 	t.Duration = duration //Сохранить полученное значение в соответствующем поле структуры Training.
 
@@ -47,7 +53,7 @@ func (t Training) ActionInfo() (string, error) {
 	var calories float64
 	var err error
 
-	switch strings.ToLower(t.TrainingType) { //Проверить, какой вид тренировки содержится в структуре Training.
+	switch t.TrainingType { //Проверить, какой вид тренировки содержится в структуре Training.
 	case "Бег": //Рассчитать калории, используя функцию из пакета spentenergy.
 		calories, err = spentenergy.RunningSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
 	case "Ходьба": //Рассчитать калории, используя функцию из пакета spentenergy.
@@ -61,7 +67,7 @@ func (t Training) ActionInfo() (string, error) {
 	}
 
 	hours := t.Duration.Hours()
-	info := fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f",
+	info := fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n",
 		t.TrainingType, hours, distance, speed, calories) //Сформируйте и верните строку, образец которой был выше.
 
 	return info, nil
